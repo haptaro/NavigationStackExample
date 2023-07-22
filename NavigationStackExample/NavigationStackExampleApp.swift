bddaf1f5ffcafe97706b5e29f62c3b240c4a345b
Kotaro Fukuo <haptaro.dev@gmail.com>
2023-07-22 10:58:12 -0700

    Initial Commit

---
+++ NavigationStackExample/NavigationStackExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackExampleApp.swift
+//  NavigationStackExample
+//
+//  Created by Kotaro Fukuo on 2023/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
